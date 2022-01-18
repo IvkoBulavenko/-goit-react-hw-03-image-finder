644a0f058f90db2b449b7cdc650be017cce4f613
Ivan Bulavenko <bulavenkoivan@gmail.com>
2022-01-18 21:34:13 +0200

    hwbagfix

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { createPortal } from 'react-dom';
+import PropTypes from 'prop-types';
+import c from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+class Modal extends Component {
+  static propTypes = {
+    onCloseModal: PropTypes.func.isRequired,
+    largeImageURL: PropTypes.string.isRequired,
+  };
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onCloseModal();
+    }
+  };
+
+  handleBackdropClick = event => {
+    if (event.currentTarget === event.target) {
+      this.props.onCloseModal();
+    }
+  };
+
+  render() {
+    const { largeImageURL } = this.props;
+
+    return createPortal(
+      <div className={c.Overlay} onClick={this.handleBackdropClick}>
+        <div className={c.Modal}>
+          <img src={largeImageURL} alt="" />
+        </div>
+      </div>,
+      modalRoot,
+    );
+  }
+}
+
+export default Modal;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css';
+import App from './components/App';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 
 ReactDOM.render(
   <React.StrictMode>
